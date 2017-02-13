6bc536ecd38ef333df20643f299d9852f57466ac
Jorge Luis Limo Arispe <jorgelimo@iMac-de-Jorge.local>
2017-02-12 23:18:43 -0500

    Initial Commit

---
+++ Tarea2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarea2
+//
+//  Created by Jorge Luis Limo Arispe on 12/02/17.
+//  Copyright © 2017 Jose Romero Quiroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
